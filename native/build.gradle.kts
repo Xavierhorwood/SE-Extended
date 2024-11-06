b572aa5846076b509c778da1c9ed0f42bd8953b2
Xavier horwood <39760456+Xavierhorwood@users.noreply.github.com>
2024-11-06 22:27:42 +1000

    Add NDK r28 beta 1 and update Rust dependencies in Cargo.lock and build.gradle.kts to latest versions

--- native/build.gradle.kts
+++ native/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     namespace = rootProject.ext["applicationId"].toString() + ".nativelib"
     compileSdk = 34
     buildToolsVersion = "34.0.0"
-    ndkVersion = "27.1.12297006"
+    ndkVersion = "28.0.12433566"
 
     buildFeatures {
         buildConfig = true
