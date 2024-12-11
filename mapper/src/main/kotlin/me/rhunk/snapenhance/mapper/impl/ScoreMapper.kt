e5297d2b6e0518d74fb5db6473702bb6bdd98b45
Xavier horwood <39760456+Xavierhorwood@users.noreply.github.com>
2024-12-12 01:19:53 +1000

    Refactor ScoreMapper to comment out old parameter type checks for improved clarity in score mapping logic

--- mapper/src/main/kotlin/me/rhunk/snapenhance/mapper/impl/ScoreMapper.kt
+++ mapper/src/main/kotlin/me/rhunk/snapenhance/mapper/impl/ScoreMapper.kt
@@ -13,7 +13,7 @@ class ScoreMapper : AbstractClassMapper("Score"){
             for (clazz in classes) {
                 val firstConstructor = clazz.directMethods.firstOrNull { it.name == "<init>" } ?: continue
                 if (firstConstructor.parameters.size != 5) continue
-                if (firstConstructor.parameterTypes[1] != "str" || firstConstructor.parameterTypes[2] != "str2") continue
+                //if (firstConstructor.parameterTypes[1] != "str" || firstConstructor.parameterTypes[2] != "str2") continue
 
                 if (clazz.methods.firstOrNull { it.name == "toString" }?.implementation?.findConstString("Result\\(userId=", contains = true) != true) continue
 
