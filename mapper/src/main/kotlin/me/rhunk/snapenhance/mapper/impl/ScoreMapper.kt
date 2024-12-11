fd3320dd837795a056060d36d43033de4964dd4d
Xavier horwood <39760456+Xavierhorwood@users.noreply.github.com>
2024-12-11 20:39:53 +1000

    Fix ScoreMapper to check for 5 parameters in constructor instead of 6 for correct scoring logic

--- mapper/src/main/kotlin/me/rhunk/snapenhance/mapper/impl/ScoreMapper.kt
+++ mapper/src/main/kotlin/me/rhunk/snapenhance/mapper/impl/ScoreMapper.kt
@@ -12,7 +12,7 @@ class ScoreMapper : AbstractClassMapper("Score"){
         mapper {
             for (clazz in classes) {
                 val firstConstructor = clazz.directMethods.firstOrNull { it.name == "<init>" } ?: continue
-                if (firstConstructor.parameters.size != 6) continue
+                if (firstConstructor.parameters.size != 5) continue
                 if (firstConstructor.parameterTypes[1] != "str" || firstConstructor.parameterTypes[2] != "str2") continue
 
                 if (clazz.methods.firstOrNull { it.name == "toString" }?.implementation?.findConstString("score", contains = true) != true) continue
